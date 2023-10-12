177c7040eb7ec89e8b9a03296fb2890504dca1b5
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-10-12 14:01:14 +0100

    More pythonic

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
+                if key in ["created_at", "updated_at"]:
                     """
                     Convert string representation of datetime to
                     datetime object
@@ -21,9 +21,15 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+            # Set 'id' if it exists in kwargs, or generate a new one
+            if 'id' not in kwargs:
+                self.id = str(uuid4())
         else:
+            # Generate an ID as a string
             self.id = str(uuid.uuid4())
+            # Current datetime when an instance is create
             self.created_at = datetime.now()
+            # Updated whenever an object is created or updated
             self.updated_at = datetime.now()
 
     def __str__(self):
@@ -33,7 +39,7 @@ class BaseModel:
         )
 
     def save(self):
-        """Updates the updated_at attribute with the current datetime."""
+        """Updates the 'updated_at' attribute with the current datetime."""
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
@@ -43,10 +49,12 @@ class BaseModel:
         Returns a dictionary containing all
         keys/values of __dict__ of the instance
         """
-        instance_dict = (  ## is the bracket necessary
-            self.__dict__.copy()
-        )  # Creating a copy to not alter the original __dict__
+        # Creating a copy to not alter the orignal __dict__
+        instance_dict = self.__dict__.copy()
         instance_dict["__class__"] = self.__class__.__name__
+
+        # Format 'created _at' and 'updated_at' as ISO strings
         instance_dict["created_at"] = self.created_at.isoformat()
         instance_dict["updated_at"] = self.updated_at.isoformat()
+
         return instance_dict
