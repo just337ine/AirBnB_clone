be92b8fcf10abf17b463f15dd9fd36c53eabd013
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-10-14 21:26:42 +0100

    find error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -81,13 +81,19 @@ class FileStorage:
             deserialization process.
             Update this dictionary as new classes are added.
             """
-            classes = {
-                    'BaseModel': BaseModel,
-                    'User': User,
-                    'State': State,
-                    'City': City,
-                    'Amenity': Amenity,
-                    'Place': Place,
-                    'Review': Review
-            }
+            from models.base_model import BaseModel
+            from models.user import User
+            from models.state import State
+            from models.city import City
+            from models.amenity import Amenity
+            from models.place import Place
+            from models.review import Review
+
+            classes = {'BaseModel': BaseModel,
+                       'User': User,
+                       'State': State,
+                       'City': City,
+                       'Amenity': Amenity,
+                       'Place': Place,
+                       'Review': Review}
             return classes
