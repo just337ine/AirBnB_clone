fc74db59052057cab3d3de33603ef56a83dbb619
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-10-14 21:35:32 +0100

    .

--- console.py
+++ console.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Module for the entry point of the command interpreter."""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
+    """Class for the command interpreter."""
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
